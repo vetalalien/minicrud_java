256d6d8167652258a701a5a7360962c3f6e7bd2e
Vitalii <i1818i@mail.ru>
2022-06-24 23:42:14 +0300

    Добавил вывод переменных окружения в лог, внёс кое-какие изменения в application.properties

--- src/main/java/com/vetalalien/miniservice/MiniserviceApplication.java
+++ src/main/java/com/vetalalien/miniservice/MiniserviceApplication.java
@@ -2,11 +2,19 @@ package com.vetalalien.miniservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+Lj
+import java.util.Map;
 
 @SpringBootApplication
 public class MiniserviceApplication {
 
 	public static void main(String[] args) {
+		Map<String, String> envs = System.getenv();
+		for (Map.Entry<String, String> entry : envs.entrySet()) {
+			System.out.println(entry.getKey() + ":" + entry.getValue());
+		}
+
 		SpringApplication.run(MiniserviceApplication.class, args);
+
 	}
 }
